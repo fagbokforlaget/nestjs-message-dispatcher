4eac3c51298b22eb5ebcfd58f447004f83a01034
Rafal Wrzochol <rafal.wrzochol@gmail.com>
2023-06-16 09:18:25 +0200

    feat: omit sending message for incomplete data

--- test/mocks/message-dispatcher-test.controller.ts
+++ test/mocks/message-dispatcher-test.controller.ts
@@ -10,6 +10,33 @@ export class MessageDispatcherTestController {
     objectIdGetter: (request) => request.params.id,
     action: Action.CREATED,
   })
+  @Get('/nodata')
+  async testNotexistant(): Promise<{ id: null }> {
+    return { id: null };
+  }
+
+  @MessageEventEmitter({
+    objectIdGetter: (request) => request.params.id,
+    action: Action.CREATED,
+  })
+  @Get('/null')
+  async testNoData(): Promise<object> {
+    return {};
+  }
+
+  @MessageEventEmitter({
+    objectIdGetter: (request) => request.params.id,
+    action: Action.CREATED,
+  })
+  @Get('/undefined')
+  async testNull(): Promise<undefined> {
+    return;
+  }
+
+  @MessageEventEmitter({
+    objectIdGetter: (request) => request.params.id,
+    action: Action.CREATED,
+  })
   @Get(':id')
   async test(@Param() params: { id: string }): Promise<{ id: string }> {
     return { id: params.id };
